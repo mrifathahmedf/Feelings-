cd740828031c031ff12ffbf96bc9dba26f9415ca
mrifathahmedf <mdrifathahmed2020@gmail.com>
2025-03-03 22:12:34 +0600

    Update script.js

--- script.js
+++ script.js
@@ -19,3 +19,10 @@ document.addEventListener("DOMContentLoaded", function () {
 
     typeText();
 });
+function showTab(tabId) {
+    let tabs = document.getElementsByClassName("tab-content");
+    for (let i = 0; i < tabs.length; i++) {
+        tabs[i].style.display = "none";
+    }
+    document.getElementById(tabId).style.display = "block";
+}
