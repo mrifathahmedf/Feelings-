d6a1a6d88b862d9f92c8bde8e445ed403b5fd94c
mrifathahmedf <mdrifathahmed2020@gmail.com>
2025-03-03 22:07:56 +0600

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 document.addEventListener("DOMContentLoaded", function () {
-    const text = `আল্লাহ আমাদেরকে “অতি আশাবাদী কিংবা হতাশাগ্রস্থ জাতি হিসেবে পরিচয় করিয়ে দেন নাই”! 
+    const text = `আল্লাহ আমাদেরকে “অতি আশাবাদী কিংবা হতাশাগ্রস্থ জাতি হিসেবে পরিচয় করিয়ে দেন নাই”!
     মহান আল্লাহ পাক আমাদের সম্পর্কে পরিচয় করিয়ে দিয়েছেন “মধ্যমপন্থী অর্থাৎ ভারসাম্যপূর্ণ জাতি" হিসেবে ☺️ (সূরা আল-বাকারা ২:১৪৩)
     
     আর পুরো কুরআন মাজিদ তো ভারসাম্যপূর্ণ ❤️
@@ -13,7 +13,7 @@ document.addEventListener("DOMContentLoaded", function () {
         if (index < text.length) {
             typingText.innerHTML += text.charAt(index);
             index++;
-            setTimeout(typeText, 50);
+            setTimeout(typeText, 50); // ৫০ms টাইপিং স্পিড
         }
     }
 
